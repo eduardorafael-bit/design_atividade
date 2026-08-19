d24652a8795e5a5412f603608aec0e8a7174c271
eduardorafael-bit <eduardo.rafael@escolar.ifrn.edu.br>
2026-08-19 18:01:42 -0300

    definitivo

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
 alert("olá mundo!");
+
+var nasc = 2009;
+let nome = "eduardo";
+const viva = true;
+let altura = 1.83;
+
+if(viva){
+    let saudacao = "ola" + nome + "!";
+    let msg = ` altura ${altura}m | idade ${2026-nasc}`;
+    alert (saudacao+"\n"+msg);
+}else{
+   alert("rip");
+}
