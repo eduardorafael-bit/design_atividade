2e62c219b7fe25993ee64c743b971d1832e3b53a
eduardorafael-bit <eduardo.rafael@escolar.ifrn.edu.br>
2026-08-26 17:16:11 -0300

    alterações

--- js/app.js
+++ js/app.js
@@ -7,10 +7,12 @@ function calcIdade(ano=2026){
     let menor;
     if(idade < 18){
         menor = true;
+        var podebeber = false;
     }else{
         menor = false;
+        var podebeber = true;
     }
-    alert(`${nome} é menor de idade? ${menor}\nidade: ${idade}`);
+    alert(`${nome} é menor de idade? ${menor}\nidade: ${idade}\npode beber: ${podebeber}`);
     return idade;
 }
 
