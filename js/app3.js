6469f74193a4f744c585096640172bfd0c480cbc
eduardorafael-bit <eduardo.rafael@escolar.ifrn.edu.br>
2026-08-26 17:50:56 -0300

    alteracao hosting

---
+++ js/app3.js
@@ -0,0 +1,15 @@
+let nasc = 2009;
+let nome = "Eduardo";
+let viva = true;
+let altura = 1.83;
+let sexo;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`ano de nasc - tipo: ${typeof(nasc)}`)
+alert(`nome - tipo: ${typeof(nome)}`)
+alert(`vivo - tipo: ${typeof(viva)}`)
+alert(`altura - tipo: ${typeof(altura)}`)
+alert(`sexo - tipo: ${typeof(sexo)}`)
+alert(`peso - tipo: ${typeof(peso)}`)
+alert(`imc - tipo: ${typeof(imc)}`)
