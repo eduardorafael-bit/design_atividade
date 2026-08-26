b2e9b0cbb40356cbf97ce98c8080b6c091ea59ac
eduardorafael-bit <eduardo.rafael@escolar.ifrn.edu.br>
2026-08-26 17:04:10 -0300

    aula 4

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ const viva = true
 
 function calcIdade(ano=2026){
     let idade = ano - nasc;
-    alert(`dentro de função - idade ${idade}`);
+    let menor;
+    if(idade < 18){
+        menor = true;
+    }else{
+        menor = false;
+    }
+    alert(`${nome} é menor de idade? ${menor}\nidade: ${idade}`);
     return idade;
 }
 
