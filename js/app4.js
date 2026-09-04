52fa5294944182f45b7f53a71404948fb25e8565
eduardorafael-bit <eduardo.rafael@escolar.ifrn.edu.br>
2026-09-04 14:26:55 -0300

    continue

--- js/app4.js
+++ js/app4.js
@@ -1,4 +1,12 @@
 let vezes = Number(prompt("digite a quantidade de vezes: "));
 for (let i = 1; i<=vezes; i++){
+    if (vezes > 100){
+        alert("valor invalido, digite um entre 0 e 100");
+        break
+    }
     alert(`contei ${i} vezes`);
+    if(i%2!=0){
+        continue;
+    }
+    alert(`${i} é par`);
 }
